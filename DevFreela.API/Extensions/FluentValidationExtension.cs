23b44f03326c12e003394255ba31d591ef60e21f
Athos Schrapett <athosschrapett@outlook.com>
2024-06-01 08:56:08 -0300

    feat: inclusão fluent validation

---
+++ DevFreela.API/Extensions/FluentValidationExtension.cs
@@ -0,0 +1,15 @@
+﻿using DevFreela.Application.Validators;
+using FluentValidation;
+using FluentValidation.AspNetCore;
+
+namespace DevFreela.API.Extensions;
+
+public static class FluentValidationExtension
+{
+    public static void AddConfigFluentValidation(this IServiceCollection services)
+    {
+        services.AddFluentValidationAutoValidation();
+        services.AddFluentValidationClientsideAdapters();
+        services.AddValidatorsFromAssemblyContaining<CreateProjectCommandValidator>();
+    }
+}
--- DevFreela.API/Program.cs
+++ DevFreela.API/Program.cs
@@ -1,22 +1,25 @@
-using DevFreela.API.Models;
+using DevFreela.API.Extensions;
+using DevFreela.API.Filters;
 using DevFreela.Application.Commands.CreateProject;
-using DevFreela.Application.Services.Implementations;
-using DevFreela.Application.Services.Interfaces;
+using DevFreela.Core.Repositories;
 using DevFreela.Infrastructure.Persistence;
+using DevFreela.Infrastructure.Persistence.Repositories;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
-builder.Services.AddControllers();
+builder.Services.AddControllers(options => options.Filters.Add(typeof(ValidationFilter)));
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var connectionString = builder.Configuration.GetConnectionString("DevFreelaCs");
 builder.Services.AddDbContext<DevFreelaDbContext>(options => options.UseSqlServer(connectionString));
 
-builder.Services.AddScoped<IProjectService, ProjectService>();
-builder.Services.AddScoped<IUserService, UserService>();
-builder.Services.AddScoped<ISkillService, SkillService>();
+builder.Services.AddScoped<IProjectRepository, ProjectRepository>();
+builder.Services.AddScoped<IUserRepository, UserRepository>();
+builder.Services.AddScoped<ISkillRepository, SkillRepository>();
+
+builder.Services.AddConfigFluentValidation();
 
 builder.Services.AddMediatR(options =>
 {
@@ -27,8 +30,9 @@ var app = builder.Build();
 
 if (app.Environment.IsDevelopment())
 {
+    app.UseDeveloperExceptionPage();
     app.UseSwagger();
-    app.UseSwaggerUI();
+    app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "DevFreela.API v1"));
 }
 
 app.UseHttpsRedirection();
